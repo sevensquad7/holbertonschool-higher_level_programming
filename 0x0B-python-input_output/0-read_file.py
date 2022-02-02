7862a6b37ac645d61cc895981d7e195818c50ca9
zye7ert <edgarxot@gmail.com>
2022-02-02 13:08:42 -0500

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import lib os"""
+import os
+""""
+Write a function that reads a text file (UTF8)
+and prints it to stdout:
+"""
+def read_file(filename=""):
+    with open("my_file_0.txt", encoding="utf-8" ) as Myfile:
+        print(Myfile.read())
