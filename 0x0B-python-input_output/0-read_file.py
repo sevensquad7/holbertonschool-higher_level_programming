d39afa1229c4beedf55b17254b0cbfb99ddd5993
zye7ert <edgarxot@gmail.com>
2022-02-02 17:14:07 -0500

    upload changed modo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ and prints it to stdout:
 
 def read_file(filename=""):
     """function that reads a text"""
-    with open(file = filename, encoding="utf8") as Myfile:
+    with open(file=filename, encoding="utf8") as Myfile:
         print(Myfile.read())
