a514f9775fa8a443b14c4c13f9c8ca7ab74369bc
zye7ert <edgarxot@gmail.com>
2022-02-02 15:45:12 -0500

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ and prints it to stdout:
 
 
 def read_file(filename=""):
+    """function that reads a text"""
     with open("my_file_0.txt", mode="r", encoding="utf-8") as Myfile:
         print(Myfile.read())
