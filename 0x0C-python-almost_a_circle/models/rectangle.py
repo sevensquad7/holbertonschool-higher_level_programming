2e83b1cfc6184b2731d6fe70ecd0a1d04fda9372
zye7ert <edgarxot@gmail.com>
2022-02-11 12:13:52 -0500

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""define Rectangle"""
+
+
+from models.base import Base
+
+class Rectangle:
+    """define class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """define contructor"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.id = id
+    
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        '''Set width'''
+        self.__width = width
+    
+    @property
+    def height(self):
+        '''Get height'''
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        '''Set height'''
+        self.__height = height
+
+    @property
+    def x(self):
+        '''Get x'''
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        '''Set x'''
+        self.__x = x
+
+    @property
+    def y(self):
+        '''Get y'''
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        '''Set y'''
+        self.__y = y
