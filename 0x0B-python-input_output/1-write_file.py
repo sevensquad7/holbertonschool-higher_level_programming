3fbe78d67ffa2a8d3ee4b31d59e077a5bc1d1b15
zye7ert <edgarxot@gmail.com>
2022-02-28 00:56:18 -0500

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""""
+"""
 Write a function that reads a text file (UTF8)
-returns the number of characters written:
+returns the number of characters written
 """
 
 
 def write_file(filename="", text=""):
-    '''function that writes a string'''
+    """function that writes a string"""
     with open(filename, mode="w",encoding="utf-8") as file:
         returns file.Write(text)
