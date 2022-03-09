3640ced8c35ff19fdd64a333c1b6b80b7bcade63
zye7ert <edgarxot@gmail.com>
2022-03-08 22:02:19 -0500

    correct to output of the code

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,6 @@
 -- Change collation in database
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 use hbtn_0c_0;
 
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
-COLLATE utf8mb4_unicode_ci NULL;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
