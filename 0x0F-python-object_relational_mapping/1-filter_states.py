dfa400ec959aba53f3db5954ecb2117290fb8a49
zye7ert <edgarxot@gmail.com>
2022-04-11 10:30:21 -0500

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                                     db=sys.argv[3])
     cur = db_connection.cursor()
     cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
-    OR 'n%' ORDER BY id ASC""")
+    OR name LIKE 'n%' ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
