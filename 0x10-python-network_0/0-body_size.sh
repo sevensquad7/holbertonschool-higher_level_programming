92958e10bbd98c8123d31409a7ed8e49d21e36df
zye7ert <edgarxot@gmail.com>
2022-04-21 20:25:51 -0500

    load file cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes The size must be displayed in bytes
-sudo curl 0.0.0.0:5000 -i -s | grep "Content-Length" | cut -d ' ' -f2
+sudo curl -i -s $1 | grep "Content-Length" | cut -d ' ' -f2
