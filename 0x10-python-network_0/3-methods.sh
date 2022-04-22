5fd270de865c38850e1ac34d237440df2fc7e3dd
zye7ert <edgarxot@gmail.com>
2022-04-21 21:16:36 -0500

    load file cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script takes in a URL and displays all HTTP method
-sudo curl -i -s $1 | grep "Allow" | cut -d ':' -f2
+sudo curl -I -s $1 | grep "Allow" | cut -d ':' -f2
