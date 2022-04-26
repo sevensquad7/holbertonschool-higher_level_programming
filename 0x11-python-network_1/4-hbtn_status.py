4351531acd185713acade312fb2f051a43f99032
zye7ert <edgarxot@gmail.com>
2022-04-26 13:46:12 -0500

    load file What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import requests
-if __name__ == "__main__":
 
-    r = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+r = requests.get('https://intranet.hbtn.io/status')
+print('Body response:')
+print('\t- type: {}'.format(type(r.text)))
+print('\t- content: {}'.format(r.text))
