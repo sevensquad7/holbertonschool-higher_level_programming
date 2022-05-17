5f2d0755d3fa911e4316fdfec6fdd1eeffb66a12
zye7ert <edgarxot@gmail.com>
2022-05-17 18:57:33 -0500

    load file Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], process.argv[3], 'utf8', errorFile);
+
+function errorFile (err, f) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(f);
+}
