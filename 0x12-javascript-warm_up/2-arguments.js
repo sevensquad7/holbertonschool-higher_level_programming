bc25a1aee50cac01ccc5fe6072c47f824cc4f2b4
zye7ert <edgarxot@gmail.com>
2022-05-17 15:56:00 -0500

    correct space in file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv[3]) {
-    console.log('Arguments found');
-}else if (process.argv[2]) {
-    console.log('Argument found');
-}else{
-    console.log('No argument');
+  console.log('Arguments found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
