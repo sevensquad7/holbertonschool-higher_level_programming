93052498afe1195e94ebd0d451b76285bf65ecb7
zye7ert <edgarxot@gmail.com>
2022-05-17 17:31:49 -0500

    load file Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const numIni = parseInt(process.argv[2]);
 const numSec = parseInt(process.argv[3]);
-let i = 0;
-
 if (Number.isNaN(numIni) || Number.isNaN(numSec)) {
   console.log(NaN);
 } else {
