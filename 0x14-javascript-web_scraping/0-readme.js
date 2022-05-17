cbbbc132054f6dc5953c854026629806a05a3b74
zye7ert <edgarxot@gmail.com>
2022-05-17 18:53:57 -0500

    load file Readme - correct standar

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', errorFile);
 
-function errorFile(err, f) {
+function errorFile (err, f) {
   if (err) {
     return console.error(err);
   }
   console.log(f);
-};
+}
