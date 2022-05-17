98d1247fb31c53cf527664a0c920d066737fb102
zye7ert <edgarxot@gmail.com>
2022-05-17 16:47:28 -0500

    load file Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arrLang = ['C is fun',
+             'Python is cool',
+             'JavaScript is amazing'];
+let i=0;
+
+while(i<arrLang.length){
+  console.log(arrLang[i]);
+  i++;
+}
