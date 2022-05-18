0bb0427ba8ba2db2187f4ce1b860ed7f4b79fb47
zye7ert <edgarxot@gmail.com>
2022-05-17 20:18:16 -0500

    load file Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const axios = require('axios');
+async function makeHeadRequest () {
+  try {
+    const res = await axios.head(process.argv[2]);
+    console.log(`Status: ${res.status}`);
+  } catch (err) {
+    console.log('code:', err.response.status);
+  }
+}
+makeHeadRequest();
