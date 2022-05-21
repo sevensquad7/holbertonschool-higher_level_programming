f615c7699fcb091f8a5e6f8f0f6d34b34013da02
zye7ert <edgarxot@gmail.com>
2022-05-21 01:15:43 -0500

    load file Who was playing in this movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+axios.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`)
+  .then(res => {
+    res.data.characters.forEach(async (data) => {
+      await axios.get(data)
+        .then(res => {
+          console.log(res.data.name);
+        });
+    });
+  })
+  .catch(err => {
+    console.log('Error:', err);
+  });
+  
