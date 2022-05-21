c624502621d276c086568da900e02895bc4e1ef4
zye7ert <edgarxot@gmail.com>
2022-05-21 01:49:11 -0500

    load file Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const numIni = parseInt(process.argv[2]);
-let count = 1;
-
-if (Number.isNaN(numIni)) {
-  console.log(1);
-} else {
-  for (let index = 1; index <= numIni; index++) {
-    count = count * index;
-  }
-  console.log(count);
+let numIni = parseInt(process.argv[2]);
+function numFact (numIni) {
+  if (num > 0) return (num * numFact(numIni));
+  return (1);
 }
+if (isNaN(numIni)) num = 1;
+console.log(numFact(numIni));
