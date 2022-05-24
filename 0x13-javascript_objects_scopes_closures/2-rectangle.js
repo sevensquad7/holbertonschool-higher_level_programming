c6bf1649c7f67cb23b98f9bdab7fb920a8d72901
zye7ert <edgarxot@gmail.com>
2022-05-23 22:51:38 -0500

    load file Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h){
-    if (h>0 && w>0 && Number.isInteger(h) && Number.isInteger(w)){
-      this.width = w
-      this.height = h
+  constructor (w, h) {
+    if (h > 0 && w > 0 && Number.isInteger(h) && Number.isInteger(w)) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
