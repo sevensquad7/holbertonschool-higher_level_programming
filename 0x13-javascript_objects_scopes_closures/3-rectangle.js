234c6a9e5b3da4a314af080f2941f352012bc40d
zye7ert <edgarxot@gmail.com>
2022-05-23 22:38:51 -0500

    load file Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h){
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)){
+      this.width = w
+      this.height = h
+      
+    }
+  }
+  print () {
+    for (let heiRec = 0; heiRec < this.height; heiRec++) {
+      for (let widRec = 0; widRec < this.width; widRec++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+}
+module.exports = Rectangle;
